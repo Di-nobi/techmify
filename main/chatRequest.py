93a6655b8b0921a248d5321d86124398349d51ba
Dinobi <architectstan01@gmail.com>
2023-11-29 16:50:47 +0100

    Added more collections for chatting to the database

--- main/chatRequest.py
+++ main/chatRequest.py
@@ -3,3 +3,8 @@ import mongoengine
 class ChatRequest(mongoengine.Document):
     from_id = mongoengine.StringField(required=True)
     to_id = mongoengine.StringField(required=True)
+
+    meta = {
+        'db_alias': 'core',
+        'collection': 'chatRequest'
+    }
