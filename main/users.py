8e4fb9768114bf20b00dcba3bf3d92b187769067
Dinobi <architectstan01@gmail.com>
2023-11-29 16:53:03 +0100

    Added more columns to the Users collection

--- main/users.py
+++ main/users.py
@@ -3,6 +3,9 @@ import datetime
 
 class MongoDBUser(mongoengine.Document):
     # id = mongoengine.IntField(required=False)
+    username = mongoengine.StringField(required=True)
+    firstname = mongoengine.StringField(required=False)
+    lastname = mongoengine.StringField(required=False)
     email = mongoengine.StringField(required=True)
     hashed_password = mongoengine.StringField(required=True)
     reset_token= mongoengine.StringField(required=False)
