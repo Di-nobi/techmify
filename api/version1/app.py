72a2b85529e6c6f0cabc7d4087272ac11d50fbb5
Dinobi <architectstan01@gmail.com>
2023-12-01 11:06:45 +0100

    Rounded up the Messaging Algorithm for the site, yet to undergo testing

--- api/version1/app.py
+++ api/version1/app.py
@@ -21,7 +21,7 @@ socketio = SocketIO(app)
 #     """Closes Store"""
 #     store.close()
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'udehdinobi@gmail.com'
--- api/version1/views/chat.py
+++ api/version1/views/chat.py
@@ -5,7 +5,9 @@ from flask import Flask, request, jsonify, abort
 from api.version1.views import socketio, app_views
 from main.engine.db import DBStorage
 from main.chatRequest import ChatRequest
+from main.chats import Message
 from main.users import MongoDBUser
+from uuid import uuid4
 key = Fernet.generate_key()
 cipher = Fernet(key)
 
@@ -16,9 +18,11 @@ def handler_req(arg):
         Handles getting the sending and receiving id
     """
     try:
+        # Gets the receivers Id from the database
         from_id = DBStorage.get(ChatRequest, arg['from_id'])
         if not from_id:
             abort(404)
+        # Gets the senders Id from the database
         to_id = DBStorage.get(ChatRequest, arg['to_id'])
         if not to_id:
             abort(404)
@@ -29,3 +33,39 @@ def handler_req(arg):
             emit('chat_request', {'from_id': from_id}, room = to_id)
     except Exception as err:
         print(f"Error occured: {err}")
+
+@socketio.on('accept_request')
+def accepts_reg(data):
+    """This functions handles accepting of a users request"""
+
+    # Gets the receivers Id from the database
+    to_id = DBStorage.get(ChatRequest, data['to_id'])
+    if not to_id:
+        abort(404)
+    # Gets the senders Id from the database
+    from_id = DBStorage.get(ChatRequest, data['from_id'])
+    if not from_id:
+        abort(404)
+    
+    room = str(uuid4())
+
+    #Displays to both the sender and the receiver that the chat has been accepted
+    emit('chat_accepted', {'room': room}, room = from_id)
+    emit('chat_accepted', {'room': room}, room=to_id)
+
+    #Adds the sender and the receiver to thesame room
+    join_room(room, sid=from_id)
+    join_room(room, sid=to_id)
+
+@socketio.on('message')
+def message_handler(args):
+    """This handles the conversation between users in the room"""
+
+    #Decrypts the message for the user to see on the clients interface
+    message_decrypt = cipher.decrypt(args['message'].encode()).decode()
+
+    #Stores the Encrypted message to the backend server for security
+    Message(content=args['message'], room=args['room']).save()
+
+    #Displays the messages
+    emit('message', {'message': message_decrypt}, room=args['room'])
